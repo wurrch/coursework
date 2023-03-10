5ecf5618e958c7ec3260706efc392241d2a858fe
wurrch <95665217+wurrch@users.noreply.github.com>
2023-03-10 20:29:16 +0000

    Can now tap on side of the screen for movement

--- Bezier Attempt/Assets/Scripts/Character/CharacterControls.cs
+++ Bezier Attempt/Assets/Scripts/Character/CharacterControls.cs
@@ -4,32 +4,43 @@ using UnityEngine;
 
 public class CharacterControls : MonoBehaviour
 {
-    private bool isTouching;
+    private bool isColliding;
     Rigidbody2D rigidbody;
     CircleCollider2D collider;
+
     void Start(){
         rigidbody = GetComponent<Rigidbody2D>();
         collider = GetComponent<CircleCollider2D>();
-
     }
     // Update is called once per frame
     void Update()
     {
-        if(isTouching){
+        if(isColliding){
             if(Input.GetKey(KeyCode.RightArrow)){
                 rigidbody.AddForce(new Vector2(5f, 0), ForceMode2D.Force);
             }
             else if(Input.GetKey(KeyCode.LeftArrow)){
                 rigidbody.AddForce(new Vector2(-5f, 0), ForceMode2D.Force);
             }
+
+            if (Input.touchCount > 0){
+                Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+
+                if(touchPos.x > 0){
+                    rigidbody.AddForce(new Vector2(5f, 0), ForceMode2D.Force);
+                }
+                else if(touchPos.x < 0){
+                    rigidbody.AddForce(new Vector2(-5f, 0), ForceMode2D.Force);
+                }
+            }
         }
     }
 
     void OnCollisionEnter2D(Collision2D collision){
-        isTouching = true;
+        isColliding = true;
     }
 
     void OnCollisionExit2D(Collision2D collision){
-        isTouching = false;
+        isColliding = false;
     }
 }
