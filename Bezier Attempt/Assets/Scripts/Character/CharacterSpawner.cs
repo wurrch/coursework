420ba2799546a32614e44573a7045ca266c7ffcc
wurrch <95665217+wurrch@users.noreply.github.com>
2023-03-15 21:14:00 +0000

    Adding characterspawner stuff

---
+++ Bezier Attempt/Assets/Scripts/Character/CharacterSpawner.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSpawner : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Bezier Attempt/Assets/Scripts/Sandbox/SandboxPlay.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandboxPlay : MonoBehaviour
+{
+    public bool sandboxCurrentlyPlaying = false;
+   // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
